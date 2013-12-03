2e4d4ba858c54c0564d590d3bf7492ae1b3835c0
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2013-12-03 12:06:30 +0330

    Spring security with 3 roles and 3 users added to project. right now users are static but i will make them dynamic. Find out users and roles in : WEB::resources/contexts/security.xml

--- web/src/main/java/com/sarabadani/onlinecademy/web/config/Initializer.java
+++ web/src/main/java/com/sarabadani/onlinecademy/web/config/Initializer.java
@@ -1,12 +1,16 @@
 package com.sarabadani.onlinecademy.web.config;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import java.util.EnumSet;
 
 /**
  * Author: Soroosh Sarabadani
@@ -22,10 +26,22 @@ public class Initializer implements WebApplicationInitializer {
         mvcContext.register(FormatterConfig.class);
         mvcContext.register(ViewConfig.class);
 
+
+
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
                 "dispatcher", new DispatcherServlet(mvcContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
+
+        servletContext.addFilter("springSecurityFilterChain", DelegatingFilterProxy.class).addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+
+       servletContext.setInitParameter("contextConfigLocation","classpath*:contexts/security.xml");
+
+        servletContext.addListener(ContextLoaderListener.class);
+
+
+
+
     }
 
 
