37cab350d28bf3b5e2870c5eb0879d5000976312
Soroosh Sarabadani <Soroosh.Sarabadani@gmail.com>
2013-12-06 22:00:12 +0330

    security bug resolved

--- web/src/test/resources/init_user.sql
+++ web/src/test/resources/init_user.sql
@@ -1,5 +1,5 @@
 insert into  user (ID,VERSION,PASSWORD,USERNAME) values (1,0,'123','soroosh')  ;
 
-insert into   USER_AUTHORITY (USER_ID,AUTHORITY) values (1,"ADMIN")   ;
-insert into   USER_AUTHORITY (USER_ID,AUTHORITY) values (1,"STUDENT")   ;
-insert into   USER_AUTHORITY (USER_ID,AUTHORITY) values (1,"TEACHER")    ;
+insert into   USER_AUTHORITY (USER_ID,AUTHORITY) values (1,'ADMIN')   ;
+insert into   USER_AUTHORITY (USER_ID,AUTHORITY) values (1,'STUDENT')   ;
+insert into   USER_AUTHORITY (USER_ID,AUTHORITY) values (1,'TEACHER')    ;
